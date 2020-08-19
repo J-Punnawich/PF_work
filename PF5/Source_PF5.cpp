92598de5617e5b3764a1c2f6871755d0aca98bb2
junio-plz <somyedofwar@gmail.com>
2020-08-19 16:37:39 +0700

    fix PF5 from sign number of age

--- PF5/Source_PF5.cpp
+++ PF5/Source_PF5.cpp
@@ -5,16 +5,17 @@ int main() {
 	int age;
 	printf("Enter your age: ");
 	scanf("%d", &age);
-
-	if (age > 0 && age <= 11) 
-		printf("Kid");
-	else if (age > 11 && age <= 20)
-		printf("Teen");
-	else if (age > 20 && age <= 60)
-		printf("Adult");
-	else
-		printf("Old");
-
+	if (age > 0) {
+		if (age > 0 && age <= 11)
+			printf("Kid");
+		else if (age > 11 && age <= 20)
+			printf("Teen");
+		else if (age > 20 && age <= 60)
+			printf("Adult");
+		else
+			printf("Old");
+	} else 
+		printf("Try again");
 
 	return 0;
 }
