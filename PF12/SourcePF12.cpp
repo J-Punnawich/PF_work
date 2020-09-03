f93242b5a1d1211e88ce0cfa31b3ac0f2a4fc710
junio-plz <somyedofwar@gmail.com>
2020-09-03 23:17:59 +0700

    fix PF12

--- PF12/SourcePF12.cpp
+++ PF12/SourcePF12.cpp
@@ -5,21 +5,24 @@ int kittung(int x);
 int main() {
 	int x;
 	scanf("%d", &x);
-	if (x >= 0) {
+	
 		kittung(x);
 
-	}
-	else printf("Error");
+	
 
 
 }
 int kittung(int x) {
+	if (x >= 0) {
 	if (x % 4 == 0) {
 		printf("%d Baht", 249 * (x - (x / 4)));
 	}
 	else {
-		x == x;
+		x = x;
 		printf("%d Baht", 249 * x);
+
 	}
+	}
+	else printf("Error");
 	return 0;
 }
