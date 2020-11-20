b40f28a7f56a6ba2f88a5dae14cb2872ee77a223
junio-plz <somyedofwar@gmail.com>
2020-11-20 22:49:43 +0700

    week14(20/11)

--- week14(20_11)/Sourceweek14.cpp
+++ week14(20_11)/Sourceweek14.cpp
@@ -5,34 +5,38 @@
 int hextodec(char n[])
 {
     int len= strlen(n);
-    int base= 1;
-    int dec= 0;
+    int base= 1 , dec = 0;
+    int* pt_dec = &dec;
 
     for (int i = len-1; i>=0; i--)
     {
-    
-        if (n[i] >= '0' && n[i] <= '9')
-        {
-            dec += (n[i] - 48) * base;
+       
+            if (n[i] >= '0' && n[i] <= '9')
+            {
+                *pt_dec += (n[i] - 48) * base;
 
-            base = base * 16;
-        }
+                base = base * 16;
+            }
 
-        else if (n[i] >= 'A' && n[i] <= 'F')
-        {
-            dec += (n[i] - 55) * base;
+            else if (n[i] >= 'A' && n[i] <= 'F')
+            {
+                *pt_dec += (n[i] - 55) * base;
 
-            base = base * 16;
-        }
+                base = base * 16;
+            }
+        
+       
     }
 
-    return dec;
+    return *pt_dec;
 }
 
 
 int main()
 {   
+
     char hexNum[5];
     scanf("%s", hexNum);
     std::cout << hextodec(hexNum) << "\n";   
+
 }
